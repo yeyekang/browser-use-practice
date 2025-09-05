f1e64493926e72b84524647a700e48ab919e1ec5
yeyekang <15249185085@163.com>
2025-09-05 20:30:49 +0800

    Update browseruse.py

--- browseruse.py
+++ browseruse.py
@@ -5,7 +5,7 @@ import asyncio
 load_dotenv()
 
 async def main():
-    llm = ChatGoogle(model="gemini-2.5-flash", api_key="AIzaSyBr7MiZT_ATcQsTiMw0X4EAgK9oCgnr0Y4")
+    llm = ChatGoogle(model="gemini-2.5-flash", api_key="把这里改成自己的api key")
     task = "请抓取豆瓣 TOP100 每部电影的类型，统计每种类型数量，然后自动生成饼图显示各类型占比。数据抓取、分析和可视化步骤全部自动完成。网站是这个：https://www.douban.com/doulist/3936288/ "
     agent = Agent(task=task, llm=llm, output_dir="/Users/kangqiman/代码库/基于browseruse开发的爬虫demo")
     await agent.run()
